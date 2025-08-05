5af92ed5a30505bea565f23f8c32b28eb00459a9
Maxmud <maxmudbobomurodov151@gmail.com>
2025-08-05 16:26:54 +0500

    s

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
 from products.models import Products
 
-# admin.site.register(Products, UserAdmin)
+admin.site.register(Products)
